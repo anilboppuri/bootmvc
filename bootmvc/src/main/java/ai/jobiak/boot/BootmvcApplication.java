1bf1779c329ab0126cf33e42ab212d859426d9ac
anilboppuri <anil4java2@gmail.com>
2022-05-15 08:21:13 +0530

    initializing code base to remote repo

---
+++ bootmvc/src/main/java/ai/jobiak/boot/BootmvcApplication.java
@@ -0,0 +1,13 @@
+package ai.jobiak.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootmvcApplication.class, args);
+	}
+
+}
